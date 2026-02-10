6888a04d9923cad48830b748113d3ceb9e1a5e74
LE ANH TUAN 180303 <leanhtuan.18.03.03@gmail.com>
2026-02-10 19:05:49 +0700

    add session

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,28 +1,28 @@
-import bcrypt from 'bcrypt';
-import User from '../models/User.js';
-import jwt from 'jsonwebtoken';
-import crypto from 'crypto';
+import bcrypt from 'bcrypt'
+import User from '../models/User.js'
+import jwt from 'jsonwebtoken'
+import crypto from 'crypto'
+import Session from '../models/Session.js'
 
-const ACCESS_TOKEN_TTL = '30m'; // 15minutes
-const REFRESH_TOKEN_TTL = '14d'; // 7 days
-
+const ACCESS_TOKEN_TTL = '30m' // 15minutes
+const REFRESH_TOKEN_TTL = 14 * 24 * 60 * 60 * 1000 // 7 days in milliseconds
 
 export const signUp = async (req, res) => {
   try {
-    const { username, email, password, firstname, lastname } = req.body;
+    const { username, email, password, firstname, lastname } = req.body
 
-    if(!username || !email || !password || !firstname || !lastname) {
-        return res.status(400).json({ message: 'All fields are required!' });
+    if (!username || !email || !password || !firstname || !lastname) {
+      return res.status(400).json({ message: 'All fields are required!' })
     }
 
     // Check if user already exists
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ email })
     if (existingUser) {
-      return res.status(409).json({ message: 'User already exists' });
+      return res.status(409).json({ message: 'User already exists' })
     }
 
     // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10)
 
     // Create a new user
     await User.create({
@@ -30,59 +30,67 @@ export const signUp = async (req, res) => {
       email,
       hashedPassword,
       displayName: `${firstname} ${lastname}`,
-    });
+    })
 
-    res.status(204).json({ message: 'User created successfully' });
+    res.status(204).json({ message: 'User created successfully' })
   } catch (error) {
-    console.error('Error during sign up: ', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error('Error during sign up: ', error)
+    res.status(500).json({ message: 'Internal server error' })
   }
-}; 
+}
 
 export const signIn = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { email, password } = req.body
 
-    if(!email || !password) {
-        return res.status(400).json({ message: 'Email and password are required!' });
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: 'Email and password are required!' })
     }
 
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email })
     if (!user) {
-      return res.status(404).json({ message: 'Invalid email' });
+      return res.status(404).json({ message: 'Invalid email' })
     }
 
-    const isPasswordValid = await bcrypt.compare(password, user.hashedPassword);
+    const isPasswordValid = await bcrypt.compare(password, user.hashedPassword)
     if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Invalid password' });
+      return res.status(401).json({ message: 'Invalid password' })
     }
 
     // Tao Access Token
     const accessToken = jwt.sign(
       { userId: user._id },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: ACCESS_TOKEN_TTL }
-    );
+      { expiresIn: ACCESS_TOKEN_TTL },
+    )
 
-    res.status(200).json({ token });
+    // Tao Refresh Token
+    const refreshToken = crypto.randomBytes(64).toString('hex')
+
+    // tao session
+    await Session.create({
+      userId: user._id,
+      refreshToken,
+      expiresAt: new Date(Date.now() + REFRESH_TOKEN_TTL),
+    })
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      maxAge: REFRESH_TOKEN_TTL, // 7 days
+    })
+
+    return res.status(200).json({
+      message: `Sign in successful: User[${user.displayName}]`,
+      accessToken,
+    })
   } catch (error) {
-    console.error('Error during sign in: ', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error('Error during sign in: ', error)
+    res.status(500).json({ message: 'Internal server error' })
   }
-
-  // Tao Refresh Token
-  const refreshToken = crypto.randomBytes(64).toString('hex');
-
-  // tao session
-  await Session.create({
-    userId: user._id,
-    refreshToken,
-    expiresAt: new Date(Date.now() + REFRESH_TOKEN_TTL),
-  })
-
-
-};
-
-export const signOut = async (req, res) => {
-
 }
+
+export const signOut = async (req, res) => {}
