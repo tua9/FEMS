7231d5d967fe435c372f42b49ac250d38bad69aa
Danh0504X <Danhvt.504.dev@gmail.com>
2026-02-27 13:30:18 +0700

    edit Google - login route

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -3,10 +3,14 @@ import {
   signIn,
   signOut,
   refreshToken,
+  googleLogin
 } from '../controllers/authController.js'
 
 const router = express.Router()
 
+
+
+router.post('/google-login', googleLogin)
 router.post('/signin', signIn)
 router.delete('/signout', signOut)
 router.post('/refresh-token', refreshToken)
--- backend/src/routes/googleRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-
-
-const router = express.Router()
-
-router.post('/google-login', authController.googleLogin)
-
-export default router
